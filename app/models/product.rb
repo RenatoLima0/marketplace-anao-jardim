118f472f6de50455a6083aa522f9f676387fb296
Renato Lima <renatoslima46@gmail.com>
2020-06-01 18:59:15 -0300

    Model validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :localization, presence: true
+  validates :disponibility, presence: true
+  validates :price, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+
 end
