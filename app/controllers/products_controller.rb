ef43b6b105b97b3f110efea37093ce88144c8495
Matheus Moreira Ferreira <moreira.mferreira@gmail.com>
2020-06-01 18:58:29 -0300

    products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,52 @@
 class ProductsController < ApplicationController
+
+  def index
+    @product = Product.all
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit 
+
+  end
+
+  def new
+    @product = Product.new
+  end
+  
+  def update
+    
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to cocktail_path(@product.cocktail)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:description, :user_id)
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
+end
+
+
 end
