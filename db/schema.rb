c023c0f8d078c92a53425f53a4def4e15be7b5e3
Matheus Moreira Ferreira <moreira.mferreira@gmail.com>
2020-06-03 16:42:10 -0300

    just for gco

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_195308) do
+ActiveRecord::Schema.define(version: 2020_06_03_181405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_06_02_195308) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
