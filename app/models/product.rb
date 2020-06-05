146abdecc3a97bc070b153fd388523cc4cf99d1d
andre <aguiar1001@hotmail.com>
2020-06-05 12:21:13 -0300

    fix the bugs

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,8 @@ class Product < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_localization?
   validates :title, presence: true
   validates :localization, presence: true
-  validates :disponibility, presence: true
-  validates :price, presence: true
+  validates :disponibility, presence: true, inclusion: { in: 1..100 }
+  validates :price, presence: true, inclusion: { in: 1..10000000000000000 }
   validates :category, presence: true
   validates :description, presence: true
 
